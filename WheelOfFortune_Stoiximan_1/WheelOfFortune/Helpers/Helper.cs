be1126355eaf2d9520d22a33a0ed929adc438764
Nick Limnios <nicklimnios@gmail.com>
2017-10-18 16:58:23 +0300

    Seed Intitial Data

---
+++ WheelOfFortune_Stoiximan_1/WheelOfFortune/Helpers/Helper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune.Helpers
+{
+    public class Helper
+    {
+        public string CalculateHash(string input)
+        {
+            var salt = GenerateSalt(16);
+
+            var bytes = KeyDerivation.Pbkdf2(input, salt, KeyDerivationPrf.HMACSHA512, 10000, 16);
+
+            return $"{ Convert.ToBase64String(salt) }:{ Convert.ToBase64String(bytes) }";
+        }
+
+
+
+        private static byte[] GenerateSalt(int length)
+        {
+            var salt = new byte[length];
+
+            using (var random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(salt);
+            }
+
+            return salt;
+        }
+
+    }
+}
