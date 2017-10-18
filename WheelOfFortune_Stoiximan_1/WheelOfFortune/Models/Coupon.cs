b6bee235ce1b9f9e9c4cf50ef4217b6702bd0d62
SillentAssembly <silentassembly2@gmail.com>
2017-10-18 14:44:11 +0300

    Added Transactions and Coupon Tables

---
+++ WheelOfFortune_Stoiximan_1/WheelOfFortune/Models/Coupon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; } //the code for the user to redeem it.
+        public float Value { get; set; } //value the user will receive after redeeming
+
+        public DateTime CreationDate { get; set; }
+        public DateTime ExpirationDate { get; set; } //in case we want the coupon to expire
+
+        public bool FlagUsed { get; set; } // if the coupon is used. Do we need this?
+
+        //do we need the ones beelow?
+
+        //public string AdminIssuedThis { get; set; }
+        //public int Uses { get; set; }
+        //public ICollection<string> UsersAbleToUseThis { get; set; }
+
+    }
+}
