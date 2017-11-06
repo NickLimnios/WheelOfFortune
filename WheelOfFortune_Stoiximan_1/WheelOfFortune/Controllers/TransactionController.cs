c4158ab94e36f0cf50a8f1d4769ed7a84d6590a6
SillentAssembly <silentassembly2@gmail.com>
2017-11-06 21:11:34 +0200

    The balance is shown in a label at the top of the site along with log out and user manage. Needs proper UI fixing but the functionality is there.

--- WheelOfFortune_Stoiximan_1/WheelOfFortune/Controllers/TransactionController.cs
+++ WheelOfFortune_Stoiximan_1/WheelOfFortune/Controllers/TransactionController.cs
@@ -19,6 +19,20 @@ namespace WheelOfFortune.Controllers
         }
 
         [Authorize]
-        public ViewResult TransactionList() => View(transactionRepository.Transactions.Where(p => p.UserId == Convert.ToInt32(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value)));
+        public ViewResult TransactionList() => View(transactionRepository.Transactions
+            .Where(p => p.UserId == Convert.ToInt32(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value)));
+
+        //Should this be moved to a service or something similar?
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetUserBalance()
+        {
+            
+            float userBalance = transactionRepository.Transactions.
+                Where(u=>u.UserId == Convert.ToInt32(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value)).
+                Sum(t => t.Amount);
+
+            return Json(new { balance = userBalance });
+        }
     }
 }
--- WheelOfFortune_Stoiximan_1/WheelOfFortune/wwwroot/js/site.js
+++ WheelOfFortune_Stoiximan_1/WheelOfFortune/wwwroot/js/site.js
@@ -1,2 +1,18 @@
 ﻿// Write your JavaScript code.
 
+//Get User balance from server
+
+function getBalance() {
+
+    $.ajax({
+        url: urls.balanceUrls,
+        type: 'GET',
+        data: { amount: 'balance' }
+
+    }).done(function (data) {
+        document.getElementById('userBalance').innerText = "My Balance is: " + data.balance;
+
+    }).fail(function () {
+        alert('fail');
+    });
+}
