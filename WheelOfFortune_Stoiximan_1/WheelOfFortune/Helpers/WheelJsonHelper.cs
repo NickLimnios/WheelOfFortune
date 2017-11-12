dbf7188f7ec7390ecd279af3f9efe2640c11b38a
Cipher <Cipher@CIPHER-PC>
2017-11-12 16:17:15 +0200

    Attempting to save and write the wheel json and each slice on server.

---
+++ WheelOfFortune_Stoiximan_1/WheelOfFortune/Helpers/WheelJsonHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WheelOfFortune.Models.Wheels;
+
+namespace WheelOfFortune.Helpers
+{
+    //add stuff at top and bottom of json and segemtns in between. return the json ready for client consumption.
+    public class WheelJsonHelper
+    {
+        public List<WheelSliceContainer> slices { get; set; }
+
+        public CreateJson(string stringToConvert)
+        {
+
+        }
+        public JsonToString() { }
+
+    }
+
+}
---
+++ WheelOfFortune_Stoiximan_1/WheelOfFortune/Models/Wheels/Wheel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune.Models.Wheels
+{
+    //When the Admin creates a wheel, this is what the DB will store.
+    public class Wheel
+    {
+        public int Id { get; set; }
+
+        public string WheelJson{ get; set; }
+    }
+}
